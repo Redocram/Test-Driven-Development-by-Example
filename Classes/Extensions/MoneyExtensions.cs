a5a0f2e882d1d71d761adf0cc8db25407e9c375c
DELTATRE\marco.antonino <marco.antonino@deltatre.it>
2018-11-19 14:57:15 +0100

    created extension method

---
+++ Classes/Extensions/MoneyExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDByExample.Classes.Extensions
+{
+  public class MoneyExtensions
+  {
+    public static Dollar Dollar(int amount)
+    {
+      return new Dollar(amount);
+    }
+  }
+}
