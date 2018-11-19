b73100027b602e816da60ec434f6e2941ae01252
DELTATRE\marco.antonino <marco.antonino@deltatre.it>
2018-11-19 11:42:46 +0100

    moved amount and Equals to Money

--- Classes/Dollar.cs
+++ Classes/Dollar.cs
@@ -2,8 +2,6 @@
 {
   public class Dollar : Money
   {
-    private int amount { get; set; }
-
     public Dollar(int amount)
     {
       this.amount = amount;
@@ -12,12 +10,6 @@
     public Dollar Times(int multiplier)
     {
       return new Dollar(amount * multiplier);
-    }
-
-    public override bool Equals(object obj)
-    {
-      Dollar dollarToCompare = (Dollar)obj;
-      return amount == dollarToCompare.amount;
     }
   }
 }
--- Classes/Money.cs
+++ Classes/Money.cs
@@ -2,5 +2,12 @@
 {
   public class Money
   {
+    protected int amount { get; set; }
+
+    public override bool Equals(object obj)
+    {
+      Money moneyToCompare = (Money)obj;
+      return amount == moneyToCompare.amount;
+    }
   }
 }
